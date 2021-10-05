7ad5f70ab3cd757d963faf82c09a62db125ca355
rafeym <mohammadrafey0@gmail.com>
2021-10-05 13:36:02 -0400

    DEVELOP: Scroll to top feature added

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react'
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Navbar from './components/Navbar/Navbar'
 
 import NotFound from './Pages/NotFound'
 
 import Home from './Pages/Home'
 import Footer from './components/Footer/Footer'
+import Header from './components/Navbar/Header'
+import ScrollToTop from './helper/ScrollToTop'
 
 function App() {
   return (
     <>
       <Router>
-        <Navbar />
+        <ScrollToTop />
+        <Header />
         <Switch>
           <Route exact path='/' component={Home} />
           <Route component={NotFound} />
