c57269aa21bed4c0163c985481d7294d8bbc899c
rafeym <mohammadrafey0@gmail.com>
2021-10-08 21:59:27 -0400

    DEVELOP: Login and user types created

---
+++ client/src/store/types/index.js
@@ -0,0 +1,10 @@
+export const LOGIN_REQUEST = 'LOGIN_REQUEST'
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const LOGIN_FAIL = 'LOGIN_FAIL'
+
+export const USER_LOADED = 'USER_LOADED'
+export const USER_FAIL = 'USER_FAIL'
+
+export const LOGOUT = 'LOGOUT'
+
+export const SET_LOADING = 'SET_LOADING'
