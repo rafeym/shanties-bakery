ff4404cbbe5ff6f0d46a0d9b51a635934a66ee79
rafeym <mohammadrafey0@gmail.com>
2021-10-22 15:50:58 -0400

    DEVELOP: pagination component

---
+++ client/src/components/Pagination/Pagination.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { BsChevronDoubleRight, BsChevronDoubleLeft } from 'react-icons/bs'
+
+const Pagination = ({ count, page, pageLimit }) => {
+  let totalPages = Math.ceil(count / pageLimit)
+  let startLoop = page
+  let diff = totalPages - page
+
+  if (diff <= 3) {
+    startLoop = totalPages - 3
+  }
+  let endLoop = startLoop + 3
+
+  if (startLoop <= 0) {
+    startLoop = 1
+  }
+
+  const links = () => {
+    const store = []
+    for (let i = startLoop; i <= endLoop; i++) {
+      store.push(
+        <li>
+          <Link to={`/inventory/${i}`}>{i}</Link>
+        </li>
+      )
+    }
+    return store
+  }
+
+  const next = () => {
+    if (page < totalPages) {
+      return (
+        <li>
+          <Link to={`/inventory/${parseInt(page) + 1}`}>
+            <BsChevronDoubleRight />
+          </Link>
+        </li>
+      )
+    }
+  }
+
+  const prev = () => {
+    if (page > 1) {
+      return (
+        <li>
+          <Link to={`/inventory/${parseInt(page) - 1}`}>
+            <BsChevronDoubleLeft />
+          </Link>
+        </li>
+      )
+    }
+  }
+
+  return totalPages ? (
+    <div className='pagination'>
+      {prev()}
+      {links()}
+      {next()}
+    </div>
+  ) : (
+    ''
+  )
+}
+
+export default Pagination
