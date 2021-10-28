740ed05b042336342e910a5141e41263a6312cc4
rafeym <mohammadrafey0@gmail.com>
2021-10-27 20:18:45 -0400

    DEVELOP: cart reducer,actions, and types complete

---
+++ client/src/store/reducers/cartReducer.js
@@ -0,0 +1,34 @@
+import { ADD_TO_CART, REMOVE_FROM_CART } from '../types'
+
+const initState = {
+  cart: [],
+}
+
+export const cartReducer = (state = initState, action) => {
+  const { type, payload } = action
+  switch (type) {
+    case ADD_TO_CART:
+      const item = payload
+      const existItem = state.cart.find((x) => x.product === item.product)
+      if (existItem) {
+        return {
+          ...state,
+          cart: state.cart.map((x) =>
+            x.product === existItem.product ? item : x
+          ),
+        }
+      } else {
+        return {
+          ...state,
+          cart: [...state.cart, item],
+        }
+      }
+    case REMOVE_FROM_CART:
+      return {
+        ...state,
+        cart: state.cart.filter((x) => x.product !== payload),
+      }
+    default:
+      return state
+  }
+}
--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { userReducer } from './userReducer'
 import { productReducer } from './productReducer'
+import { cartReducer } from './cartReducer'
 
 export default combineReducers({
   userReducer,
   productReducer,
+  cartReducer,
 })
