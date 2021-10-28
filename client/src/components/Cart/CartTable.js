c62e3cd3f7de4046f1f9ffc5d9687824e1b25862
rafeym <mohammadrafey0@gmail.com>
2021-10-27 20:13:35 -0400

    DEVELOP: redux integration with cart page complete

--- client/src/components/Cart/CartTable.js
+++ client/src/components/Cart/CartTable.js
@@ -2,55 +2,76 @@ import React from 'react'
 
 import './CartTable.scss'
 
-import img1 from '../../static/product/img1.JPG'
+import { Link } from 'react-router-dom'
 
-const CartTable = () => {
+const CartTable = ({ cart, qtyChangeHandler, removeProduct, getCartTotal }) => {
   return (
     <div className='c-small-container cart-container'>
-      <table>
-        <tr>
-          <th>Product</th>
-          <th>Quantity</th>
-          <th>Subtotal</th>
-        </tr>
-        <tr>
-          <td>
-            <div className='cart-info'>
-              <img src={img1} alt='' />
-              <div>
-                <p>Product Name</p>
-                <small>Price: $50.00</small>
-                <br />
-                <a href=''>Remove</a>
-              </div>
-            </div>
-          </td>
-          <td>
-            <input type='number' value={1} />
-          </td>
-          <td>$50.00</td>
-        </tr>
-      </table>
+      {cart.length === 0 ? (
+        <div className='empty-cart-title'>
+          <h2>Your cart is empty 🛒</h2>{' '}
+          <Link className='empty-cart-btn' to='/menu'>
+            Go back
+          </Link>
+        </div>
+      ) : (
+        <>
+          <table>
+            <tr>
+              <th>Product</th>
+              <th>Quantity</th>
+              <th>Subtotal</th>
+            </tr>
+            {cart.map((item) => (
+              <>
+                <tr>
+                  <td>
+                    <div className='cart-info'>
+                      <Link to={`/product/${item.product}`}>
+                        <img src={item.image} alt={item.name} />
+                      </Link>
+                      <div>
+                        <p>{item.name}</p>
+                        <small>Price: ${item.price}</small>
+                        <br />
+                        <div
+                          className='remove-cta'
+                          onClick={() => removeProduct(item.product)}
+                        >
+                          Remove
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    <input
+                      type='number'
+                      min={1}
+                      value={item.qty}
+                      onChange={(e) =>
+                        qtyChangeHandler(item.product, e.target.value)
+                      }
+                    />
+                  </td>
+                  <td>${item.price}</td>
+                </tr>
+              </>
+            ))}
+          </table>
 
-      <div className='total-price'>
-        <table>
-          <tr>
-            <td>Subtotal</td>
-            <td>$200.00</td>
-          </tr>
-          <tr>
-            <td>Tax</td>
-            <td>$30.00</td>
-          </tr>
-          <tr>
-            <td>Total</td>
-            <td>$230.00</td>
-          </tr>
-        </table>
-      </div>
-      <div className='checkout-btn-container'>
-        <div className='checkout-btn'>Proceed to Checkout</div>
-      </div>
+          <div className='total-price'>
+            <table>
+              <tr>
+                <td>Total</td>
+                <td>${getCartTotal().toFixed(2)}</td>
+              </tr>
+            </table>
+          </div>
+          <div className='checkout-btn-container'>
+            <div className='checkout-btn'>Proceed to Checkout</div>
+          </div>
+        </>
+      )}
     </div>
   )
 }
