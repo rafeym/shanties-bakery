ff8781247489d7a7ddcfa866beedb928d6eff96d
Mohammad Rafey <mohammadrafey@Mohammads-MacBook-Pro.local>
2021-10-29 01:09:47 -0400

    DEVELOP: added toast when add to cart button is clicked on product card component

--- client/src/components/Products/ProductCard.js
+++ client/src/components/Products/ProductCard.js
@@ -3,38 +3,48 @@ import { Link } from 'react-router-dom'
 
 import { FaShoppingBag } from 'react-icons/fa'
 
+import { toast } from 'react-toastify'
 import './ProductCard.scss'
 
 import { useDispatch } from 'react-redux'
 import { addProductToCartAction } from '../../store/actions/cartActions'
 
 const ProductCard = ({ name, price, img, id }) => {
-  const dispatch = useDispatch()
+    const dispatch = useDispatch()
 
-  return (
-    <div className='product-box'>
-      <Link to={`/product/${id}`} className='product-info-image'>
-        <img src={img} alt={name} />
-      </Link>
-      <strong>
-        <Link to={`/product/${id}`} className='product-link-name'>
-          {name}
-        </Link>
-      </strong>
-      <span className='p-quantity'>Dozen</span>
-      <span className='p-price'>${price}</span>
-      <div
-        className='cart-btn'
-        onClick={() => dispatch(addProductToCartAction(id, 1))}
-      >
-        <FaShoppingBag className='cart-btn-icon' />
-        Add To Cart
-      </div>
-      <Link to={`/product/${id}`} className='view-btn'>
-        View Product
-      </Link>
-    </div>
-  )
+    const handleAddToCart = () => {
+        dispatch(addProductToCartAction(id, 1))
+        toast('Item added to cart!', {
+            position: 'bottom-center',
+            autoClose: 1200,
+            hideProgressBar: true,
+            closeOnClick: true,
+            draggable: true,
+            pauseOnHover: true,
+        })
+    }
+
+    return (
+        <div className='product-box'>
+            <Link to={`/product/${id}`} className='product-info-image'>
+                <img src={img} alt={name} />
+            </Link>
+            <strong>
+                <Link to={`/product/${id}`} className='product-link-name'>
+                    {name}
+                </Link>
+            </strong>
+            <span className='p-quantity'>Dozen</span>
+            <span className='p-price'>${price}</span>
+            <div className='cart-btn' onClick={handleAddToCart}>
+                <FaShoppingBag className='cart-btn-icon' />
+                Add To Cart
+            </div>
+            <Link to={`/product/${id}`} className='view-btn'>
+                View Product
+            </Link>
+        </div>
+    )
 }
 
 export default ProductCard
