6106b9ccf2214e75da5e5ed3a857a048ca4caf0a
rafeym <mohammadrafey0@gmail.com>
2021-11-15 13:18:05 -0500

    DEVELOP: added private dashboard routes to update order status

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,8 @@ import Cart from './Pages/Cart'
 import Contact from './Pages/Contact'
 import Login from './Pages/Login'
 import NotFound from './Pages/NotFound'
+import Product from './Pages/Product'
+import Checkout from './Pages/Checkout'
 
 import Admin from './Pages/Dashboard/Home'
 import Orders from './Pages/Dashboard/Orders'
@@ -19,8 +21,9 @@ import Completed from './Pages/Dashboard/Completed'
 import Inventory from './Pages/Dashboard/Inventory'
 import CreateProduct from './Pages/Dashboard/CreateProduct'
 import UpdateProduct from './Pages/Dashboard/UpdateProduct'
-import Product from './Pages/Product'
-import Checkout from './Pages/Checkout'
+import UpdateOrder from './Pages/Dashboard/UpdateOrder'
+import UpdateDelivery from './Pages/Dashboard/UpdateDelivery'
+import OrderDetails from './Pages/Dashboard/OrderDetails'
 
 import ScrollToTop from './helper/ScrollToTop'
 import { ToastContainer } from 'react-toastify'
@@ -32,61 +35,64 @@ import CartPrivateRoute from './private/CartPrivateRoute'
 import setAuthToken from './helper/setAuthToken'
 
 if (localStorage.token) {
-    setAuthToken(localStorage.token)
+  setAuthToken(localStorage.token)
 }
 
 function App() {
-    return (
-        <>
-            <Provider store={store}>
-                <Router>
-                    <ToastContainer />
-                    <ScrollToTop />
-                    <Switch>
-                        <Route exact path='/' component={Home} />
-                        <Route exact path='/menu/:page?' component={Menu} />
-                        <Route exact path='/product/:id' component={Product} />
-                        <Route exact path='/faq' component={Info} />
-                        <Route exact path='/cart' component={Cart} />
-                        <Route exact path='/contact' component={Contact} />
-                        <RouteLinks path='/login' exact component={Login} />
-                        <CartPrivateRoute
-                            path='/checkout'
-                            exact
-                            component={Checkout}
-                        />
-                        <PrivateRoute
-                            path='/dashboard'
-                            exact
-                            component={Admin}
-                        />
-                        <PrivateRoute path='/orders' exact component={Orders} />
-                        <PrivateRoute
-                            path='/completed'
-                            exact
-                            component={Completed}
-                        />
-                        <PrivateRoute
-                            path='/inventory/:page?'
-                            exact
-                            component={Inventory}
-                        />
-                        <PrivateRoute
-                            path='/create-product'
-                            exact
-                            component={CreateProduct}
-                        />
-                        <PrivateRoute
-                            path='/update-product/:id'
-                            exact
-                            component={UpdateProduct}
-                        />
-                        <Route component={NotFound} />
-                    </Switch>
-                </Router>
-            </Provider>
-        </>
-    )
+  return (
+    <>
+      <Provider store={store}>
+        <Router>
+          <ToastContainer />
+          <ScrollToTop />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/menu/:page?' component={Menu} />
+            <Route exact path='/product/:id' component={Product} />
+            <Route exact path='/faq' component={Info} />
+            <Route exact path='/cart' component={Cart} />
+            <Route exact path='/contact' component={Contact} />
+            <RouteLinks path='/login' exact component={Login} />
+            <CartPrivateRoute path='/checkout' exact component={Checkout} />
+            <PrivateRoute path='/dashboard' exact component={Admin} />
+            <PrivateRoute path='/orders/:page?' exact component={Orders} />
+            <PrivateRoute
+              path='/orders/update-order-status/:id'
+              exact
+              component={UpdateOrder}
+            />
+            <PrivateRoute
+              path='/orders/update-delivery-status/:id'
+              exact
+              component={UpdateDelivery}
+            />
+            <PrivateRoute
+              path='/orders/order-details/:id'
+              exact
+              component={OrderDetails}
+            />
+            <PrivateRoute path='/completed' exact component={Completed} />
+            <PrivateRoute
+              path='/inventory/:page?'
+              exact
+              component={Inventory}
+            />
+            <PrivateRoute
+              path='/create-product'
+              exact
+              component={CreateProduct}
+            />
+            <PrivateRoute
+              path='/update-product/:id'
+              exact
+              component={UpdateProduct}
+            />
+            <Route component={NotFound} />
+          </Switch>
+        </Router>
+      </Provider>
+    </>
+  )
 }
 
 export default App
