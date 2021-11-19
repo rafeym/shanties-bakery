afc7f7a2f406ba2954a9795f229b035e6a8e2c25
rafeym <mohammadrafey0@gmail.com>
2021-11-19 14:06:17 -0500

    REFACTOR: changed product details page layout and design

--- client/src/components/Product/Product.js
+++ client/src/components/Product/Product.js
@@ -40,38 +40,37 @@ const Product = ({ product, history }) => {
   }
 
   return (
-    <div className='small-container single-product'>
-      <div className='row'>
-        <div className='col-2'>
+    <div className='product-detail-container'>
+      <div className='prod-details'>
+        <div className='big-img'>
           <img src={cloudinary_secure_url} alt={name} />
         </div>
-        <div className='col-2'>
-          <h1>{name}</h1>
-          <h4>${price}</h4>
 
-          <div className='select-container'>
-            <div className='count'>
-              <h3 className='count-title'>Quantity</h3>
-              <div className='count-content'>
-                <span onClick={qtyMinus}>-</span>
-                <span>{qty}</span>
-                <span onClick={qtyPlus}>+</span>
+        <div className='prod-details-box'>
+          <div className='details-row'>
+            <h2>{name}</h2>
+            <span>${price}</span>
+          </div>
+
+          <h3>Description</h3>
+          <p>{description}</p>
+          <h3>Allergens</h3>
+          <p>{allergens}</p>
+          <h3>Serving</h3>
+          <p>Dozen</p>
+          <div className='quantity-section'>
+            <div className='qty-container'>
+              <div className='count'>
+                <div className='count-content'>
+                  <span onClick={qtyMinus}>-</span>
+                  <span>{qty}</span>
+                  <span onClick={qtyPlus}>+</span>
+                </div>
               </div>
             </div>
-          </div>
-          <div className='btn' onClick={addToCartHandler}>
-            Add to Cart
-          </div>
-          <div className='prod-details'>
-            <p>
-              DESC <span className='detail-text'>{description}</span>
-            </p>
-            <p>
-              ALLERGENS <span className='detail-text'>{allergens}</span>
-            </p>
-            <p>
-              SERVING <span className='detail-text'>Dozen</span>
-            </p>
+            <div className='btn' onClick={addToCartHandler}>
+              Add to Cart
+            </div>
           </div>
         </div>
       </div>
