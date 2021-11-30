b07c40ae5496744dccf783451eae29979fdeb8bc
rafeym <mohammadrafey0@gmail.com>
2021-11-30 16:54:00 -0500

    REFACTOR: Contact component wasnt responsive, and changed info components max-height prop

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -8,15 +8,15 @@ const Contact = () => {
   const [click, setClick] = useState(false)
   const [click1, setClick1] = useState(false)
   return (
-    <div className='contact-container'>
-      <h1>Contact Options</h1>
+    <section className='contact-container'>
+      <h2>Contact</h2>
       <div className={click ? 'contact-option active' : 'contact-option'}>
         <div className='contact-option-title'>
           <h3 onClick={() => setClick(!click)}>Email</h3>
           <FaAngleDown onClick={() => setClick(!click)} />
         </div>
         <div className='contact-option-info'>
-          <p>shantiesbakery@gmail.com</p>
+          <p className='contact-info-p'>shantiesbakery@gmail.com</p>
         </div>
       </div>
 
@@ -26,10 +26,10 @@ const Contact = () => {
           <FaAngleDown onClick={() => setClick1(!click1)} />
         </div>
         <div className='contact-option-info'>
-          <p>(416) 274-6016</p>
+          <p className='contact-info-p'>(416) 274-6016</p>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
 
