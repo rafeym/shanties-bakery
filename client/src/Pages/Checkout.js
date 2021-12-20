5f23c51cb1fd1d9263575da4209a001377533a95
rafeym <mohammadrafey0@gmail.com>
2021-12-20 16:40:54 -0500

    REFACTOR: changed input to a select tag due to the issue on mobile allowing users to enter a value of choice

--- client/src/Pages/Checkout.js
+++ client/src/Pages/Checkout.js
@@ -181,15 +181,23 @@ const Checkout = () => {
                           </div>
                         </td>
                         <td>
-                          <input
-                            type='number'
-                            min={1}
-                            max={10}
-                            value={item.qty}
+                          <select
                             onChange={(e) =>
                               qtyChangeHandler(item.product, e.target.value)
                             }
-                          />
+                            value={item.qty}
+                          >
+                            <option value={1}>1</option>
+                            <option value={2}>2</option>
+                            <option value={3}>3</option>
+                            <option value={4}>4</option>
+                            <option value={5}>5</option>
+                            <option value={6}>6</option>
+                            <option value={7}>7</option>
+                            <option value={8}>8</option>
+                            <option value={9}>9</option>
+                            <option value={10}>10</option>
+                          </select>
                         </td>
                         <td>${item.price}</td>
                       </tr>
